4534ef04f894d3f8f5103854cce85746785c981b
Lucas Nithael <lucasnithael0101@gmail.com>
2024-04-11 17:00:12 -0300

    feat: lista de registro e importação

---
+++ src/services/home.js
@@ -0,0 +1,9 @@
+import api from './api'
+
+export function getRegistroImportacao() {
+    return api
+      .get('registroimportacao/listar')
+      .then((response) => response.data)
+      .catch((error) => console.error(error))
+  }
+  
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,4 +1,45 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted} from 'vue'
+import { getRegistroImportacao } from '@/services/home'
+import { RiArrowLeftFill, RiArrowRightFill } from '@remixicon/vue'
+import usePagination from '@/utils/pagination'
+
+
+const registros = ref([])
+
+async function getRegistros(){
+  try{
+    const response = await getRegistroImportacao()
+    registros.value = response.data
+  }catch(e){
+    console.error(e)
+  }
+}
+
+// ------------------- Paginação
+const paginationRegistros = usePagination(registros, 5);
+
+const {
+  currentPage: currentPage,
+  paginatedItems: paginatedItems,
+  nextPage: nextPage,
+  previousPage: previousPage,
+  totalPages: totalPages,
+} = paginationRegistros;
+
+function formatarData(data) {
+      const dataObj = new Date(data);
+      const dia = String(dataObj.getDate()).padStart(2, '0');
+      const mes = String(dataObj.getMonth() + 1).padStart(2, '0');
+      const ano = dataObj.getFullYear();
+      return `${dia}/${mes}/${ano}`;
+    }
+
+onMounted(() => {
+  getRegistros()
+})
+
+</script>
 
 <template>
   <table class="min-w-full bg-white shadow-md rounded-xl">
@@ -10,48 +51,22 @@
       </tr>
     </thead>
     <tbody class="text-blue-gray-900">
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Despesa</td>
-        <td class="py-3 px-4">18/03/2024</td>
-        <td class="py-3 px-4">Fulano 1</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Receita</td>
-        <td class="py-3 px-4">15/03/2024</td>
-        <td class="py-3 px-4">Fulano 3</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Licitações</td>
-        <td class="py-3 px-4">14/03/2024</td>
-        <td class="py-3 px-4">Fulano 1</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Contratos</td>
-        <td class="py-3 px-4">14/03/2024</td>
-        <td class="py-3 px-4">Fulano 1</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Ata de Registro de Preço</td>
-        <td class="py-3 px-4">14/03/2024</td>
-        <td class="py-3 px-4">Fulano 1</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Renúncias Fiscais</td>
-        <td class="py-3 px-4">14/03/2024</td>
-        <td class="py-3 px-4">Fulano 3</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Obras</td>
-        <td class="py-3 px-4">13/03/2024</td>
-        <td class="py-3 px-4">Fulano 1</td>
-      </tr>
-      <tr class="border-b border-blue-gray-200">
-        <td class="py-3 px-4">Duodécimo</td>
-        <td class="py-3 px-4">13/03/2024</td>
-        <td class="py-3 px-4">Fulano 2</td>
+      <tr v-for="registro in paginatedItems" :key="registro.idRegistroImportacao" class="border-b border-blue-gray-200">
+        <td class="py-3 px-4">{{ registro.txtDestinoImportacao }}</td>
+        <td class="py-3 px-4">{{ formatarData(registro.dtImportacao) }}</td>
+        <td class="py-3 px-4">{{ registro.txtNomeUsuario }}</td>
       </tr>
     </tbody>
   </table>
+  <div class="flex items-center justify-center p-2">
+      <button @click="previousPage" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded" :disabled="currentPage === 1">
+        <RiArrowLeftFill></RiArrowLeftFill>
+      </button>
+            <span class="px-5 py-2">Página {{ currentPage }} de {{ totalPages }}</span>
+      <button @click="nextPage" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded" :disabled="currentPage === totalPages">
+        <RiArrowRightFill></RiArrowRightFill>
+      </button>
+  </div>
 </template>
 
 <style scoped></style>
