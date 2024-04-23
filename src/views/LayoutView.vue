4fd833a38b41e1b5fb0d665db7896a60335286d7
matusilva <viniciusmatheus.dev@gmail.com>
2024-04-23 16:10:33 -0300

    feat: reformular telas sidebar, e header

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,20 +1,25 @@
 <script setup>
+import { ref } from 'vue'
 import HeaderComponent from '../components/layouts/HeaderComponent.vue'
-import DashboardComponent from '../components/layouts/DashboardComponent.vue'
 import BreadcrumbsComponent from '../components/layouts/BreadcrumbsComponent.vue'
+import SidebarComponent from '@/components/layouts/SidebarComponent.vue'
+
+const showSidebar = ref(false)
+
+function toggleSidebar() {
+  showSidebar.value = !showSidebar.value
+}
 </script>
 
 <template>
-  <HeaderComponent />
-  <div class="grid grid-cols-6 gap-2 bg-gray-50 pb-10">
-    <DashboardComponent />
-    <div class="p-4 col-span-5">
-      <BreadcrumbsComponent />
-      <div class="mt-6">
-        <div class="flex flex-col">
-          <div class="overflow-x-auto">
-            <RouterView />
-          </div>
+  <div class="relative flex min-h-screen">
+    <SidebarComponent :dataShowSidebar="showSidebar" />
+    <div class="flex-1">
+      <HeaderComponent :toggleSidebar="toggleSidebar" />
+      <div class="p-8 text-gray-800">
+        <BreadcrumbsComponent />
+        <div class="my-8">
+          <RouterView />
         </div>
       </div>
     </div>
