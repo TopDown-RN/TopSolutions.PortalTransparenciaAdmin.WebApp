f29a21e6309d621fbd8b4bcc0f9db28e22e867df
Lucas Nithael <lucasnithael0101@gmail.com>
2024-06-03 14:03:30 -0300

    tela de importação de dados via planilha

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import UsuariosRoutes from '@/router/usuarios'
 import ArquivosRoutes from '@/router/arquivos'
 import MenusRoutes from '@/router/menus'
 import OuvidoriaRoutes from '@/router/ouvidoria'
+import ImportacaoPlanilhaRoutes from '@/router/importacao-planilha'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,8 @@ const router = createRouter({
     ...AutenticacaoRoutes,
     ...ArquivosRoutes,
     ...MenusRoutes,
-    ...OuvidoriaRoutes
+    ...OuvidoriaRoutes,
+    ...ImportacaoPlanilhaRoutes
   ]
 })
 
