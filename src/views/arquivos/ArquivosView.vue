595be5ef772959fc0847003f258d4e266368a118
matusilva <viniciusmatheus.dev@gmail.com>
2024-06-13 09:39:31 -0300

    refactor: remover debug

--- src/views/arquivos/ArquivosView.vue
+++ src/views/arquivos/ArquivosView.vue
@@ -444,7 +444,6 @@ onMounted(() => {
             panelClass="text-sm"
             :invalid="!id_Menu && !isValid"
           />
-          {{ id_Menu }}
           <small v-if="!id_Menu && !isValid" class="text-red-600">
             Obrigatório selecionar um menu
           </small>
